7a3b3c803582a3a03861d0698c71b8cdcd253585
mgr-inz-rafal <rchabowski@gmail.com>
2020-01-11 13:19:21 +0100

    Formatting

--- src/axon_input.rs
+++ src/axon_input.rs
@@ -1,6 +1,6 @@
 #[typetag::serde(tag = "type")]
 pub(crate) trait AxonInput {
-  fn get_value(&self) -> f64;
-  fn get_id(&self) -> Option<usize>;
-  fn get_weight(&self) -> f64;
+    fn get_value(&self) -> f64;
+    fn get_id(&self) -> Option<usize>;
+    fn get_weight(&self) -> f64;
 }
