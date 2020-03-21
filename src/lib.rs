abde7a56cd6603f55f97fa6f32a8021140c02a37
mgr-inz-rafal <rchabowski@gmail.com>
2020-03-21 13:20:12 +0100

    Rewrite internal network structure

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,8 @@ extern crate approx;
 #[macro_use]
 extern crate if_chain;
 
-pub mod axon;
-pub mod layer;
 pub mod network;
 pub mod neuron;
-pub mod neuron_repository;
-pub mod neuron_value;
 
 use rand_distr::Normal;
 
--- src/neuron.rs
+++ src/neuron.rs
@@ -1,56 +1,12 @@
-use super::axon::Axon;
-use super::layer::Layer;
-use super::neuron_value::Valued;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub(crate) enum InputKind {
-    Value(#[serde(skip)] Option<Box<dyn FnMut() -> f64>>),
-    Axon(Axon),
-}
-
-#[derive(Serialize, Deserialize)]
 pub(crate) struct Neuron {
-    pub(crate) inputs: Vec<InputKind>,
-    #[serde(skip_deserializing, skip_serializing)]
-    pub(crate) value: Option<Box<dyn Valued>>,
+    value: f64,
 }
 
 impl Neuron {
-    pub fn new() -> Neuron {
-        Neuron {
-            inputs: Vec::new(),
-            value: None,
-        }
-    }
-}
-
-pub(crate) struct NeuronBuilder<'a> {
-    layer: Option<&'a Layer>,
-}
-
-impl<'a> NeuronBuilder<'a> {
-    pub fn new() -> NeuronBuilder<'a> {
-        NeuronBuilder { layer: None }
-    }
-
-    pub fn with_connection_to_layer(mut self, layer: Option<&'a Layer>) -> Self {
-        self.layer = layer;
-        self
-    }
-
-    #[allow(clippy::borrowed_box)]
-    pub fn build(self, randomizer: &mut Box<(dyn FnMut() -> f64 + 'static)>) -> Neuron {
-        let mut neuron = Neuron::new();
-        if let Some(layer) = self.layer.as_ref() {
-            {
-                layer.neurons.iter().for_each(|n| {
-                    neuron
-                        .inputs
-                        .push(InputKind::Axon(Axon::new(*n, (randomizer)())));
-                });
-            }
-        }
-        neuron
+    pub(crate) fn new() -> Neuron {
+        Neuron { value: 0.0 }
     }
 }
