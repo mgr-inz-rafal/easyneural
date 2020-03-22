2b9ffc621dd1c760665f18ce31f6270521d08dfe
mgr-inz-rafal <rchabowski@gmail.com>
2020-03-22 18:12:07 +0100

    Support activation function

--- src/network.rs
+++ src/network.rs
@@ -3,20 +3,28 @@ use crate::randomizer::{DefaultRandomizer, FixedRandomizer, RandomProvider};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct Network {
+pub struct NetworkLayout {
     neurons: Vec<Neuron>,
     layers: Vec<Vec<usize>>,
 }
 
+pub struct Network {
+    layout: NetworkLayout,
+    activator: fn(f64) -> f64,
+}
+
 impl Network {
-    fn new(layer_count: usize) -> Network {
+    fn new(layer_count: usize, activator: fn(f64) -> f64) -> Network {
         Network {
-            neurons: Vec::new(),
-            layers: {
-                let mut layers = Vec::new();
-                layers.resize(layer_count, Vec::new());
-                layers
+            layout: NetworkLayout {
+                neurons: Vec::new(),
+                layers: {
+                    let mut layers = Vec::new();
+                    layers.resize(layer_count, Vec::new());
+                    layers
+                },
             },
+            activator,
         }
     }
 
@@ -27,7 +35,6 @@ impl Network {
             .zip(input_values.iter())
             .for_each(|(neuron_id, input_value)| {
                 neurons[*neuron_id].value = Some(*input_value);
-                println!("{}, {}", neuron_id, input_value);
             });
     }
 
@@ -37,6 +44,7 @@ impl Network {
         prev_layer: &Vec<usize>,
         neurons: &mut Vec<Neuron>,
         is_last: bool,
+        activator: fn(f64) -> f64,
     ) {
         for i in 0..layer.len() - if is_last { 0 } else { 1 } {
             let mut value = 0.0;
@@ -50,35 +58,39 @@ impl Network {
                     .expect("Neuron w/o value found");
                 value += input_value * prev_layer_neuron_value;
             }
-            neurons[neuron_index].value = Some(value);
+            neurons[neuron_index].value = Some(activator(value));
         }
     }
 
     #[allow(dead_code)]
     fn fire(&mut self, input_values: &[f64]) {
         assert!(
-            self.layers.len() > 0,
+            self.layout.layers.len() > 0,
             "Trying to fire network with no layers"
         );
         assert_eq!(
             input_values.len(),
-            self.layers[0].len() - 1,
+            self.layout.layers[0].len() - 1,
             "Incorrent number of inputs"
         );
 
-        Network::set_layer_values(&mut self.layers[0], input_values, &mut self.neurons);
-        for layer_index in 1..self.layers.len() {
+        Network::set_layer_values(
+            &mut self.layout.layers[0],
+            input_values,
+            &mut self.layout.neurons,
+        );
+        for layer_index in 1..self.layout.layers.len() {
             Network::fire_layer(
-                &self.layers[layer_index],
-                &self.layers[layer_index - 1],
-                &mut self.neurons,
-                if layer_index == self.layers.len() - 1 {
+                &self.layout.layers[layer_index],
+                &self.layout.layers[layer_index - 1],
+                &mut self.layout.neurons,
+                if layer_index == self.layout.layers.len() - 1 {
                     true
                 } else {
                     false
                 },
+                self.activator,
             );
-            println!();
         }
     }
 }
@@ -86,6 +98,7 @@ impl Network {
 pub struct NetworkBuilder<'a> {
     neurons_per_layer: Option<&'a [usize]>,
     randomizer: Option<&'a mut dyn RandomProvider>,
+    activator: Option<fn(f64) -> f64>,
 }
 
 impl<'a> NetworkBuilder<'a> {
@@ -93,6 +106,7 @@ impl<'a> NetworkBuilder<'a> {
         NetworkBuilder {
             neurons_per_layer: None,
             randomizer: None,
+            activator: None,
         }
     }
 
@@ -106,29 +120,39 @@ impl<'a> NetworkBuilder<'a> {
         self
     }
 
+    pub fn with_activator(&mut self, activator: fn(f64) -> f64) -> &mut Self {
+        self.activator = Some(activator);
+        self
+    }
+
     pub fn build(&mut self) -> Network {
         if let Some(neurons_per_layer) = self.neurons_per_layer {
-            let mut net = Network::new(neurons_per_layer.len());
-            for layer_index in 0..neurons_per_layer.len() {
-                for _ in 0..neurons_per_layer[layer_index] {
-                    let neurons_on_previous_layer = if layer_index == 0 {
-                        0
-                    } else {
-                        neurons_per_layer[layer_index - 1] + 1
-                    };
-                    net.neurons.push(Neuron::new(
-                        false,
-                        neurons_on_previous_layer,
-                        &mut self.randomizer,
-                    ));
-                    net.layers[layer_index].push(net.neurons.len() - 1);
+            if let Some(activator) = self.activator {
+                let mut net = Network::new(neurons_per_layer.len(), activator);
+                for layer_index in 0..neurons_per_layer.len() {
+                    for _ in 0..neurons_per_layer[layer_index] {
+                        let neurons_on_previous_layer = if layer_index == 0 {
+                            0
+                        } else {
+                            neurons_per_layer[layer_index - 1] + 1
+                        };
+                        net.layout.neurons.push(Neuron::new(
+                            false,
+                            neurons_on_previous_layer,
+                            &mut self.randomizer,
+                        ));
+                        net.layout.layers[layer_index].push(net.layout.neurons.len() - 1);
+                    }
+
+                    if layer_index != neurons_per_layer.len() - 1 {
+                        net.layout.neurons.push(Neuron::new(true, 0, &mut None));
+                        net.layout.layers[layer_index].push(net.layout.neurons.len() - 1);
+                    }
                 }
-                if layer_index != neurons_per_layer.len() - 1 {
-                    net.neurons.push(Neuron::new(true, 0, &mut None));
-                    net.layers[layer_index].push(net.neurons.len() - 1);
-                }
+                net
+            } else {
+                panic!("No activator defined");
             }
-            net
         } else {
             panic!("Neurons per layer not set");
         }
@@ -146,6 +170,7 @@ mod tests {
         let net = NetworkBuilder::new()
             .with_neurons_per_layer(&neurons_per_layer)
             .with_randomizer(&mut randomizer)
+            .with_activator(|x| x)
             .build();
 
         let mut expected_neurons: Vec<usize> = neurons_per_layer.iter().map(|x| x + 1).collect();
@@ -153,24 +178,25 @@ mod tests {
             *last -= 1;
         }
 
-        assert_eq!(net.layers.len(), neurons_per_layer.len());
+        assert_eq!(net.layout.layers.len(), neurons_per_layer.len());
 
-        net.layers
+        net.layout
+            .layers
             .iter()
             .zip(expected_neurons.iter())
             .for_each(|(x, y)| assert_eq!(x.len(), *y));
 
-        for layer_id in 0..net.layers.len() - 1 {
-            let layer = &net.layers[layer_id];
+        for layer_id in 0..net.layout.layers.len() - 1 {
+            let layer = &net.layout.layers[layer_id];
             let last_neuron_id = *layer.last().expect("Layer empty");
-            let last_neuron = &net.neurons[last_neuron_id];
+            let last_neuron = &net.layout.neurons[last_neuron_id];
             assert!(relative_eq!(
                 last_neuron.value.expect("Neuron w/o value"),
                 BIAS_VALUE
             ));
         }
 
-        let serialized = serde_json::to_string(&net).unwrap();
+        let serialized = serde_json::to_string(&net.layout).unwrap();
         println!("{}", serialized);
     }
 
@@ -181,39 +207,40 @@ mod tests {
         let mut net = NetworkBuilder::new()
             .with_neurons_per_layer(&neurons_per_layer)
             .with_randomizer(&mut randomizer)
+            .with_activator(|x| x)
             .build();
 
         net.fire(&[3.7, -2.8]);
 
         assert!(relative_eq!(
-            net.neurons[2].value.unwrap(),
+            net.layout.neurons[2].value.unwrap(),
             crate::BIAS_VALUE
         ));
         assert!(relative_eq!(
-            net.neurons[6].value.unwrap(),
+            net.layout.neurons[6].value.unwrap(),
             crate::BIAS_VALUE
         ));
         assert!(relative_eq!(
-            net.neurons[3].value.unwrap(),
+            net.layout.neurons[3].value.unwrap(),
             1.5 * 3.7 + 3.0 * -2.8 + 4.5 * 1.0
         ));
         assert!(relative_eq!(
-            net.neurons[4].value.unwrap(),
+            net.layout.neurons[4].value.unwrap(),
             6.0 * 3.7 + 7.5 * -2.8 + 9.0 * 1.0
         ));
         assert!(relative_eq!(
-            net.neurons[5].value.unwrap(),
+            net.layout.neurons[5].value.unwrap(),
             10.5 * 3.7 + 12.0 * -2.8 + 13.5 * 1.0
         ));
         assert!(relative_eq!(
-            net.neurons[7].value.unwrap(),
+            net.layout.neurons[7].value.unwrap(),
             (1.5 * 3.7 + 3.0 * -2.8 + 4.5 * 1.0) * 15.0
                 + (6.0 * 3.7 + 7.5 * -2.8 + 9.0 * 1.0) * 16.5
                 + (10.5 * 3.7 + 12.0 * -2.8 + 13.5 * 1.0) * 18.0
                 + 19.5
         ));
 
-        let serialized = serde_json::to_string(&net).unwrap();
+        let serialized = serde_json::to_string(&net.layout).unwrap();
         println!("{}", serialized);
     }
 }
