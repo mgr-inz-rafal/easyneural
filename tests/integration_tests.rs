367f6527c4d806848fcc33a69935e04f693c8671
mgr-inz-rafal <rchabowski@gmail.com>
2020-03-28 16:51:50 +0100

    Basic world simulation

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -9,18 +9,24 @@ enum SpecimenStatus {
 
 struct World {
     tick: usize,
+    liveliness: isize,
 }
 
 impl World {
     pub fn new() -> World {
-        World { tick: 0 }
+        World {
+            tick: 0,
+            liveliness: 0,
+        }
     }
 
     pub fn drop(&mut self, specimen: &mut Specimen) {
+        self.tick = 0;
         loop {
             specimen.brain.fire(&[1.0, 2.0]);
             let outcome = specimen.brain.get_output();
             if let SpecimenStatus::DEAD = self.process_inputs(&outcome) {
+                println!("Specimen died at tick {}", self.tick);
                 break;
             }
         }
@@ -28,21 +34,22 @@ impl World {
 
     fn process_inputs(&mut self, inputs: &[f64]) -> SpecimenStatus {
         self.tick += 1;
-        SpecimenStatus::DEAD
+        self.liveliness = self.liveliness + if inputs[0] < 0.5 { -1 } else { 1 };
+        match self.liveliness {
+            -5..=5 => SpecimenStatus::ALIVE,
+            _ => SpecimenStatus::DEAD,
+        }
     }
 }
 
 #[test]
-fn run_training_session() -> Result<(), String> {
+fn test_run_training_session() -> Result<(), String> {
     const POPULATION_SIZE: usize = 10;
 
     let mut world = World::new();
 
     let neurons_per_layer = [2, 4, 5, 1];
     let mut session = Trainer::new(POPULATION_SIZE, &neurons_per_layer)?;
-
-    let input1 = 0.0;
-    let input2 = 1.1;
 
     session.population.iter_mut().for_each(|specimen| {
         world.drop(specimen);
