b0b1905c91352b212fa2c0798cfbd5a5a95ad18d
mgr-inz-rafal <rchabowski@gmail.com>
2020-04-25 15:52:07 +0200

    Add missing docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,30 @@ use serde::Deserialize;
 mod genetic;
 pub(crate) mod network;
 mod neuron;
+
+/// Randomizer implementation.
 pub mod randomizer;
+
+/// World used for training the network.
 pub mod simulating_world;
+
+/// Lerning routines.
 pub mod simulation;
+
+/// Interfacing with `easyneural`.
 pub mod specimen;
-/// Training ground for testing the trained network
+
+/// Training ground for testing the trained network.
 pub mod training_ground;
 
 const BIAS_VALUE: f64 = 1.0;
 const MINIMUM_POPULATION_SIZE: usize = 4;
 
+/// Specimen is used to exchange the neural network data
+/// with the outside world.
+///
+/// This is the struct you use for transferring the
+/// neural network instances to and from the `easyneural` crate.
 #[derive(Clone, Debug, Deserialize)]
 pub struct Specimen {
     pub brain: network::NetworkLayout,
